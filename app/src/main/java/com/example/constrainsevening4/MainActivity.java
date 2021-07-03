51265c9f1c407a0f42ba6a11f08c4ba5c4a53607
nalin <nalinikanta7978@gmail.com>
2021-07-03 22:49:40 +0530

    c1

---
+++ app/src/main/java/com/example/constrainsevening4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.constrainsevening4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.masaischool.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
